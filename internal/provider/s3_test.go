ec7d9b6c8cda6a68d30e9d406386af1102ee6d95
homei007 <homei007@users.noreply.github.com>
2026-08-03 23:45:43 +0100

    test: make S3 provider tests deterministic

--- internal/provider/s3_test.go
+++ internal/provider/s3_test.go
@@ -1,31 +1,33 @@
 package provider
 
 import (
-	"log"
 	"testing"
+	"time"
 
-	cfg "github.com/seacrew/helm-compose/internal/config"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func TestS3List(t *testing.T) {
-	config := &cfg.Storage{
-		Name:              "test",
-		NumberOfRevisions: 5,
-		S3Bucket:          "helm-compose",
-		S3Prefix:          "test",
-		S3Region:          "eu-central-1",
+func TestS3ProviderMinMax(t *testing.T) {
+	provider := S3Provider{name: "test"}
+	objects := []*s3.Object{
+		{Key: aws.String("other.v9.hcstate")},
+		{Key: aws.String("test.v2.hcstate"), LastModified: aws.Time(time.Unix(2, 0))},
+		{Key: aws.String("test.v5.hcstate"), LastModified: aws.Time(time.Unix(5, 0))},
+		{Key: aws.String("test.v3.hcstate"), LastModified: aws.Time(time.Unix(3, 0))},
 	}
 
-	provider, err := newS3Provider(config)
-
+	minimum, maximum, latest, err := provider.minMax(objects)
 	if err != nil {
-		log.Fatal(err)
+		t.Fatalf("minMax returned an error: %v", err)
 	}
-
-	revisions, err := provider.list()
-	if err != nil {
-		log.Fatal(err)
+	if minimum != 2 {
+		t.Fatalf("expected minimum revision 2, got %d", minimum)
 	}
-
-	log.Fatal(revisions)
+	if maximum != 5 {
+		t.Fatalf("expected maximum revision 5, got %d", maximum)
+	}
+	if latest != objects[2] {
+		t.Fatalf("expected latest object %p, got %p", objects[2], latest)
+	}
 }
